56b1393ce0c6908aabf6d0e138de3bd5789870d5
ADVallespir <advallespir@gmail.com>
2021-04-28 10:24:57 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Guia_1_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fechaValida();
+
+        }
+
+        private static void fechaValida()
+        {
+            while (true)
+            {
+                string fecha;
+                Console.WriteLine("Ingrese una fecha valida");
+                fecha = Console.ReadLine();
+                bool validaFecha = DateTime.TryParse(fecha, out DateTime fechaValida);
+                if (!validaFecha)
+                {
+                    Console.WriteLine("Fecha invalida reintente");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("La fecha es correcta");
+                    Console.WriteLine(fechaValida);
+                    break;
+                }
+            }
+        }
+    }
+}
